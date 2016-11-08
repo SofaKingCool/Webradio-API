5c76ad7ddb2eaeb123765974d1c46b06d1ca0394
Necktrox <marek.kulik@hotmail.com>
2016-11-08 11:32:25 +0100

    Added simple YouTube streaming

--- service/YouTube.php
+++ service/YouTube.php
@@ -2,6 +2,8 @@
 
 class YouTube
 {
+    const supported_itags = array(18, 22, 37, 38);
+
     private $api_key;
 
     public function __construct()
@@ -37,6 +39,49 @@ class YouTube
 
     public function url($id)
     {
+        $url = "http://www.youtube.com/get_video_info?";
+        
+        $url .= http_build_query([
+            "video_id" => $id,
+            "asv" => "3",
+            "el" => "detailpage",
+        ]);
+        
+        $video_info = array();
+        parse_str(file_get_contents($url), $video_info);
+
+        if (!isset($video_info["url_encoded_fmt_stream_map"])) {
+            return false;
+        }
+        
+        $url_encoded_fmt_stream_map = $video_info["url_encoded_fmt_stream_map"];
+        $url_encoded_fmt_streams = explode(",", $url_encoded_fmt_stream_map);
+
+        foreach ($url_encoded_fmt_streams as $encoded_stream) {
+            $stream = array();
+            parse_str($encoded_stream, $stream);
+            
+            $mime = strtok($stream["type"], ";");
+            $itag = intval($stream["itag"]);
+            
+            if ($mime == "video/mp4" && in_array($itag, self::supported_itags) && !isset($stream["s"])) {
+                $sources[] = array(
+                    "url" => urldecode($stream["url"]),
+                    "itag" => $itag,
+                );
+            }
+        }
+
+        usort($sources, function ($a, $b) {
+            return $a["itag"] > $b["itag"];
+        });
+        
+        foreach ($sources as $source) {
+            if (alive($source["url"])) {
+                return $source["url"];
+            }
+        }
+        
         return false;
     }
 }
